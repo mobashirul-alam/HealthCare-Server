b0ffd52832ca63efba725d06dce03dcdf5f2ffb9
Md. Mobashirul Alam Seam <mobashirulalamseam@gmail.com>
2024-10-14 13:39:18 +0600

    Create Admin and also a user for admin created

---
+++ src/app.ts
@@ -0,0 +1,20 @@
+import cors from "cors";
+import express, { Application, Request, Response } from "express";
+import { userRoutes } from "./app/modules/User/user.routes";
+
+const app: Application = express();
+app.use(cors());
+
+// parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req: Request, res: Response) => {
+    res.send({
+        message: "Health care project server.",
+    });
+});
+
+app.use("/api/v1/user", userRoutes);
+
+export default app;
---
+++ src/app/modules/User/user.ts
@@ -0,0 +1,11 @@
+// import express, { Request, Response } from "express";
+
+// const router = express.Router();
+
+// router.get("/", (req: Request, res: Response) => {
+//     res.send({
+//         message: "Route is working perfectly",
+//     });
+// });
+
+// export const userRoutes = router;
