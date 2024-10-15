39b0bf3fbf14c978c1b62661e8a5a39369248370
Md. Mobashirul Alam Seam <mobashirulalamseam@gmail.com>
2024-10-15 21:09:11 +0600

    Search optimized at admin service

--- src/app/modules/Admin/admin.service.ts
+++ src/app/modules/Admin/admin.service.ts
@@ -1,25 +1,27 @@
-import { PrismaClient } from "@prisma/client";
+import { Prisma, PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
 const getAllAdminFromDb = async (params: any) => {
+    const andConditions: Prisma.AdminWhereInput[] = [];
+
+    const adminSearchableFields = ["name", "email"];
+
+    if (params.searchTerm) {
+        andConditions.push({
+            OR: adminSearchableFields.map((field) => ({
+                [field]: {
+                    contains: params.searchTerm,
+                    mode: "insensitive",
+                },
+            })),
+        });
+    }
+
+    const whereConditions: Prisma.AdminWhereInput = { AND: andConditions };
+
     const result = await prisma.admin.findMany({
-        where: {
-            OR: [
-                {
-                    name: {
-                        contains: params.searchTerm,
-                        mode: "insensitive",
-                    },
-                },
-                {
-                    email: {
-                        contains: params.searchTerm,
-                        mode: "insensitive",
-                    },
-                },
-            ],
-        },
+        where: whereConditions,
     });
     return result;
 };
